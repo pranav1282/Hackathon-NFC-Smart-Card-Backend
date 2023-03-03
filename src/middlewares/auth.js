c85f69862f29fc9065b7deab2547aad0d7382731
Pranav Shukla <pranav.shukla1282@gmail.com>
2023-03-04 01:12:30 +0530

    Implemented image functionality

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY;
-// const SECRET_KEY = "NOTESAPI"
+// const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = "NOTESAPI";
 
 const auth = (req, res, next) => {
   try {
